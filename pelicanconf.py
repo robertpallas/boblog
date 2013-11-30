b4d71d3d320b333ea32c5dfcb8d35604350280a9
robertpallas <robert.pallas@igame.com>
2013-11-30 15:02:45 +0200

    site url update

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Robert Pallas'
 SITENAME = u'Boblog'
-SITEURL = ''
+SITEURL = 'http://robertpallas.github.io/boblog'
 
 GITHUB_URL = 'http://github.com/robertpallas/'
 
